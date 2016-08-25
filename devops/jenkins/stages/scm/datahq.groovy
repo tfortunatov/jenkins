c4c9efc174f86ea546029fb9f326225d8e7b0404
Taras Fortunatov <Taras_Fortunatov@epam.com>
2016-08-25 18:07:14 +0300

    Modify file for scm change

--- devops/jenkins/stages/scm/datahq.groovy
+++ devops/jenkins/stages/scm/datahq.groovy
@@ -1,16 +1,4 @@
 /**
- * Gets information about changed services and branch
- * @return  branch
+ * Proceeding with CI/CD process
  */
-def getSCMInfo(jobName, jobBuildNumber) {
-  node('master') {
-
-    def branchFile = "/tmp/${jobName}_${jobBuildNumber}_datahq_branch"
-    def branchFileHandler = new File(branchFile)
-    this.branch = branchFileHandler.text
-
-    sh "rm -f /tmp/${jobName}_${jobBuildNumber}_datahq_branch"
-  }
-}
-
-return this;
+echo 'Proceeding with CI/CD process'
