356392159165425de42281b66710859a92381817
tfortunatov <tfortunatov@gmail.com>
2016-08-25 20:32:44 +0300

    Update Jenkinsfile_datahq.groovy

--- devops/jenkins/Jenkinsfile_datahq.groovy
+++ devops/jenkins/Jenkinsfile_datahq.groovy
@@ -71,9 +71,9 @@ node('master') {
 
   
   stage "Checkout from repository"
-checkout([$class: 'GitSCM', branches: [[name: '*/${branch}']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '80116385-8f9c-4d73-875b-95510e3ee8e9', url: 'https://github.com/tfortunatov/jenkins.git']]])
+// checkout([$class: 'GitSCM', branches: [[name: '*/${branch}']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '80116385-8f9c-4d73-875b-95510e3ee8e9', url: 'https://github.com/tfortunatov/jenkins.git']]])
 
- // checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "80116385-8f9c-4d73-875b-95510e3ee8e9", url: 'https://github.com/tfortunatov/jenkins.git']]])
+  checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "80116385-8f9c-4d73-875b-95510e3ee8e9", url: 'https://github.com/tfortunatov/jenkins.git']]])
 echo "HERAK!"
   stage 'Build application'
   sh "ls -la"
